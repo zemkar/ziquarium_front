161c16c445ce887d8689cf8b0cd9b8efbc8c88d1
zemkar <zemcharu@gmail.com>
2023-03-11 21:13:43 +0200

    to deploy fix

--- src/service/constants.js
+++ src/service/constants.js
@@ -3,8 +3,8 @@
 export const PROTOCOL_HTTPS = false;
 export const STRIPE_KEY = 'pk_test_51MecxZK7eRk186VKGGCEA0WjB48f3jI8e74geU7HEfKbMk4enptqEb9rd4UvzVZhJMUx7ZDPWZ0HRa0f3Paf1B8t00QPnTpws4';
 
-const SERVER =  process.env.REACT_APP_URL ? process.env.REACT_APP_URL : "http://127.0.0.1:8000";
-// : "https://ziquarium.onrender.com/api";
+const SERVER =  process.env.REACT_APP_URL ? process.env.REACT_APP_URL : "https://ziquarium-api.onrender.com"
+// : "http://127.0.0.1:8000";
 // : "http://localhost:8000/";
 // : "http://192.168.1.123:8000";
 
