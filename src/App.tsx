e77bef45caf95d0bc329db541372e0cc5bc2738f
Anton Brodsky <zemcharu@gmail.com>
2023-03-14 14:22:59 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ function App() {
     // console.log("filled", isFilled.current);
   }, [fishes, plants, isFilled, dispatch])
 
-console.log(process.env.REACT_APP_URL)
+console.log(process.env)
   return (
     <div className="App">
       <Navbar />
